263de30717df61a8cd0e655347341b1401adeb60
Caleb Musau <calebmuteti4@gmail.com>
2024-04-22 14:26:26 +0300

    API Data

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,37 +1,39 @@
-#!/usr/bin/python3
-
-"""[task 0, get rest api]
-"""
 import requests
-from sys import argv
+import sys
 
 
-def get_user(id):
-    """get the user
-    Args:
-        id (integer: user id]
-    """
-    url = 'https://jsonplaceholder.typicode.com/'
-    users = requests.get(url + 'users', params={'id': id}).json()
-    name = users[0]['name']
-    url = 'https://jsonplaceholder.typicode.com/'
-    todos = requests.get(url + 'todos', params={'userId': id}).json()
-    return([name, todos])
+def get_user(user_id):
+    try:
+        user_response = requests.get(f'https://jsonplaceholder.typicode.com/users/{user_id}')
+        user_response.raise_for_status()
+        user_data = user_response.json()
+        
+        todos_response = requests.get(f'https://jsonplaceholder.typicode.com/todos', params={'userId': user_id})
+        todos_response.raise_for_status()
+        todos_data = todos_response.json()
+        
+        return user_data, todos_data
+    except requests.exceptions.RequestException as e:
+        print(f"Error: {e}")
+        sys.exit(1)
 
 
-def show(data):
-    name = data[0]
-    todos = data[1]
-    n = 0
-    str_to_print = ''
-    for task in todos:
-        if task['completed'] is True:
-            n += 1
-            str_to_print += '\t ' + task['title'] + '\n'
-    print('Employee {} is done with tasks({}/{}):'.format(name, n, len(todos)))
-    print(str_to_print, end='')
+def show_progress(user, todos):
+    completed_tasks = [task for task in todos if task['completed']]
+    total_tasks = len(todos)
+    num_completed_tasks = len(completed_tasks)
+    
+    print(f"Employee {user['name']} is done with tasks ({num_completed_tasks}/{total_tasks}):")
+    
+    for task in completed_tasks:
+        print(f"\t {task['title']}")
 
 
-if __name__ == '__main__':
-    data = get_user(argv[1])
-    show(data)
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Usage: python script_name.py <user_id>")
+        sys.exit(1)
+    
+    user_id = sys.argv[1]
+    user, todos = get_user(user_id)
+    show_progress(user, todos)
